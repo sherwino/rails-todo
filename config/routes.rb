2c5781d0b5950a1535547756484dfbaeef9776fb
sherwino <sherwination@gmail.com>
2016-11-06 06:45:02 +0000

    Added todo list and items destroy doesn't work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :todo_lists do
+    resources :todo_items
+  end
+  
+  root "todo_lists#index"
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +60,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
